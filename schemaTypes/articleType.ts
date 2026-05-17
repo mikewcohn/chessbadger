a41aea3d95e6f4e8ff576022b26cfb4132129f63
mikewcohn <83422536+mikewcohn@users.noreply.github.com>
2026-05-17 10:21:49 -0700

    Add embedded Sanity Studio

---
+++ schemaTypes/articleType.ts
@@ -0,0 +1,30 @@
+import {defineField, defineType} from 'sanity'
+
+export const articleType = defineType({
+  name: 'article',
+  title: 'Article',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+  ],
+})
