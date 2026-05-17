8008e4a32dc88437cfcf1cdf1b928016042f29eb
mikewcohn <83422536+mikewcohn@users.noreply.github.com>
2026-05-17 14:01:36 -0700

    Make homepage editable in Sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,9 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {schemaTypes} from './schemaTypes'
 
+const singletonTypes = new Set(['homePage'])
+const singletonActions = new Set(['publish', 'discardChanges', 'restore'])
+
 export default defineConfig({
   name: 'default',
   title: 'ChessBadger',
@@ -9,9 +12,41 @@ export default defineConfig({
   projectId: '5759b7nb',
   dataset: 'production',
 
-  plugins: [structureTool()],
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('Content')
+          .items([
+            S.listItem()
+              .title('Home Page')
+              .id('homePage')
+              .child(
+                S.document()
+                  .schemaType('homePage')
+                  .documentId('homePage')
+              ),
+
+            S.divider(),
+
+            ...S.documentTypeListItems().filter(
+              (listItem) => listItem.getId() !== 'homePage'
+            ),
+          ]),
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
+
+    templates: (templates) =>
+      templates.filter(({schemaType}) => !singletonTypes.has(schemaType)),
+  },
+
+  document: {
+    actions: (input, context) =>
+      singletonTypes.has(context.schemaType)
+        ? input.filter(({action}) => action && singletonActions.has(action))
+        : input,
   },
 })
